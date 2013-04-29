4154790dbd226d1a345277a3ed4d111f903a775f
Uli Köhler <ulikoehler@online.de>
2013-04-29 23:11:20 +0200

    Fix MSS

--- 1/MSS/src/main/java/mss/App.java
+++ 1/MSS/src/main/java/mss/App.java
@@ -1,5 +1,6 @@
-package de.btronik.mss;
+package mss;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.base.Splitter;
@@ -37,6 +38,9 @@ public class App {
             if (args[currentArgIndex].equals("3")) {
                 System.err.println("Skipping argument '3' because it's totally irrelevant (0xE*3 however is)");
                 currentArgIndex++;
+            } else if (args[currentArgIndex].equals("42") || "0xE*3".equals(args[currentArgIndex]) || "0x2a".equalsIgnoreCase(args[currentArgIndex])) {
+                System.err.println("You finally found the answer to life, the universe and everything! Why do you still bother running this stupid little program?");
+                currentArgIndex++;
             }
             if (!args[currentArgIndex++].equals("-i")) {
                 System.err.println("Error: -i not given or argument order incorrect");
@@ -47,14 +51,18 @@ public class App {
             String inputFile = args[currentArgIndex++];
             System.err.println("Reading from " + inputFile);
             AlgorithmType type;
+            MSSAlgorithm algorithm;
             //Check array bounds
             String typeString = args[currentArgIndex];
             if (typeString.equals("-n")) {
                 type = AlgorithmType.NAIVE;
+                algorithm = new NaiveAlgorithm();
             } else if (typeString.equals("-l")) {
                 type = AlgorithmType.LINEAR;
+                algorithm = new LinearAlgorithm();
             } else if (typeString.equals("-w")) {
                 type = AlgorithmType.OTHER;
+                algorithm = new RecursiveAlgorithm();
             } else {
                 System.err.println("Error: Illegal algorithm type option '" + typeString + "' given");
                 System.err.println("Usage: java -jar ukth_blatt01.jar 3 -i <Inputfile> [-n|-l|-w]");
@@ -66,8 +74,9 @@ public class App {
             lines = Collections2.filter(lines, commentFilter);
             //For each (non-comment) line, execute the algorithm
             for (String line : lines) {
-                Collection<String> stringData = Lists.newLinkedList(Splitter.on('\t').split(line));
+                Collection<String> stringData = Lists.newLinkedList(Splitter.on(CharMatcher.WHITESPACE).trimResults().omitEmptyStrings().split(line));
                 Collection<Double> data = Collections2.transform(stringData, stringToDouble);
+                System.out.println(algorithm.calculate(Lists.newArrayList(data)));
             }
         } catch (ArrayIndexOutOfBoundsException ex) {
             System.err.println("Error: Missing required arguments (argument count too small). Did you forget to include the totally irrelevant '3' argument?");
--- 1/MSS/src/test/java/mss/MSSAlgorithmTest.java
+++ 1/MSS/src/test/java/mss/MSSAlgorithmTest.java
@@ -2,8 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.btronik.mss;
+package mss;
 
+import mss.LinearAlgorithm;
+import mss.MSSAlgorithm;
+import mss.NaiveAlgorithm;
+import mss.MSSResult;
+import mss.RecursiveAlgorithm;
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import static junit.framework.Assert.assertEquals;
@@ -20,12 +25,20 @@ import static org.junit.Assert.*;
  */
 public class MSSAlgorithmTest {
 
-    static void test(MSSAlgorithm algorithm) {
+    static void test1(MSSAlgorithm algorithm) {
         ArrayList<Double> data = Lists.newArrayList(5.0, -2.0, 5.0, -2.0, 1.0, -9.0, 5.0, -2.0, 4.0, -5.0, 1.0, -2.0, 3.0, -1.0, 5.0, -3.0, 2.0, -1.0, 2.0);
         MSSResult result = algorithm.calculate(data);
         assertEquals(0, result.startIndex);
         assertEquals(2, result.endIndex);
         assertEquals(8, result.score, 0.0000001);
+    }
+
+    static void test2(MSSAlgorithm algorithm) {
+        ArrayList<Double> data = Lists.newArrayList(-1.0, 0.0, 2.0, -1.0, 2.0, 3.0, -5.0, 3.0);
+        MSSResult result = algorithm.calculate(data);
+        assert (1 == result.startIndex || 2 == result.startIndex);
+        assertEquals(5, result.endIndex);
+        assertEquals(6, result.score, 0.0000001);
     }
 
     public MSSAlgorithmTest() {
@@ -49,16 +62,19 @@ public class MSSAlgorithmTest {
 
     @Test
     public void testNaive() {
-        test(new NaiveAlgorithm());
+        test1(new NaiveAlgorithm());
+        test2(new NaiveAlgorithm());
     }
 
     @Test
     public void testLinear() {
-        test(new LinearAlgorithm());
+        test1(new LinearAlgorithm());
+        test2(new LinearAlgorithm());
     }
 
     @Test
     public void testOther() {
-        test(new RecursiveAlgorithm());
+        test1(new RecursiveAlgorithm());
+        test2(new RecursiveAlgorithm());
     }
 }
